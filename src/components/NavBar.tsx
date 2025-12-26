d837a8d7aa83bbc51b57c91f0a10a45f6b7bda12
AbdulWasay2568 <abdulwasaydogar2002@gmail.com>
2025-12-26 19:46:53 +0500

    Images renamed to images

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 // import { Link } from 'react-router-dom';
 import { Menu, X } from 'lucide-react';
-import logo from '../assets/Images/logo.jpeg'
+import logo from '../assets/images/logo.jpeg'
 
 export default function NavBar() {
   const [isOpen, setIsOpen] = useState(false);
